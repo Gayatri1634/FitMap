d1b0b5ee6e0e1ab41fabfec6f7ed422e68be1d61
Gayatri <dethegayatri@gmail.com>
2025-07-24 12:26:40 +0530

    Frontend and backend connected

---
+++ src/backend/frontend/script.js
@@ -0,0 +1,10 @@
+function getWelcomeMessage() {
+  fetch('http://127.0.0.1:5000/')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('response').innerText = data;
+    })
+    .catch(error => {
+      document.getElementById('response').innerText = 'Error connecting to backend';
+    });
+}
